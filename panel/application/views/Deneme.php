afd5d3ddb264e2b2b9f31c56b397635ae3570f65
Mustafa Emre Ilhan <mustafa.emre@btidanismanlik.com>
2020-11-28 09:56:33 +0300

    deneme dosyasında giris kontrolü yapıldı

--- panel/application/views/Deneme.php
+++ panel/application/views/Deneme.php
@@ -9,6 +9,43 @@
 
 	<?php $this->DenemeModel->get_categories(); ?>
 
+	<hr>
+
+
+ 						<?php if ($this->session->oturum_data['user']) {?>
+
+
+
+							<li class="dropdown first">							
+								<ul class="dropdown-menu level1">
+									<li> <a href="#">Anasayfa</a></li>
+									<li> <a href="#">Hakkımızda</a></li>
+									<li> <a href="#">Blog</a></li>
+									<li> <a href="#">İletişim</a></li>
+									<li><a href="<?php echo base_url('uyeislemleri/cikisyap'); ?>">Çıkış Yap</a></li>
+								</ul>
+							</li>
+
+
+
+
+
+						<?php } else { ?>
+
+							<li class="loginLink"><a href="#">Giriş Yap</a></li>
+							<li class="btn signupLink"><a href="#">Üye Ol</a></li>
+						<?php } ?>
+ 	
+
+
+	
+
+
+
+
+
+
+
 <!-- <table border="1">
 	
 <thead> <th>id</th> <th>parent_id</th> <th>name</th></thead>
