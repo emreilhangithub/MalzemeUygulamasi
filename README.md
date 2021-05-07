dd9893f214ebbed3dfb66d89aaf8c19f00234683
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-07 17:37:46 +0300

    readme eklendi db tasındı

---
+++ README.md
@@ -0,0 +1,24 @@
+# MaterialApplication
+
+## Proje Tanıtımı 
+
+*Merhaba  bu projemde **Codeigniter** kullanarak  ilk MVC projemi geliştirdim*
+
+## Php Nedir 
+
+*PHP internet için üretilmiş, sunucu taraflı, çok geniş kullanımlı, genel amaçlı, içerisine HTML gömülebilen betik ve programlama dilidir.*
+
+## Codeigniter Nedir
+
+*CodeIgniter, PHP ile dinamik uygulamalar geliştirmek için geliştirilmiş bir web uygulama iskeletidir.*
+
+## MVC Nedir
+
+*Model-View-Controller, yazılım mühendisliğinde kullanılan bir "mimari desen"dir.*
+
+### Kaynaklar ###
+https://codeigniter.com/userguide3/index.html
+
+*Eğitim ve katkılarından dolayı Kablosuz Kedi Youtube kanalına teşekkür ederim*
+
+```Php``` ```Codeigniter``` ```MVC``` ```Software``` ```Computer``` ```Programmer``` 
--- panel/application/views/search/search_list/breadcrubm.php
+++ panel/application/views/search/search_list/breadcrubm.php
@@ -6,6 +6,19 @@
                      Haberler
                    </h3>
 
+                   <h2>
+                   Haber Ara...
+                   <form action="<?php echo base_url("search/skeyword"); ?>" method="post">
+                   <div class="input-append search-input-area">
+                                   
+                               
+                   <input class="" id="appendedInputButton" type="text" name="title">
+                   <!-- <input type="submit" name="submit" value="Arama Yap"> -->
+                   <button class="btn" type="submit"  onclick="return confirm('Arama yapmak istediğinizden emin misiniz ?');"><i class="icon-search"></i> </button>
+                   </div>
+                   </form>
+                   </h2>
+
                    <?php if ($this->session->flashdata('aramabasarili')
 
                   ) {?>
